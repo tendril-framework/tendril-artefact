15cc2e051fd4dad8b80084077be0487f26d33159
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-12-11 13:04:39 +0530

    Implement artefact owner controller

--- src/tendril/artefacts/db/controller.py
+++ src/tendril/artefacts/db/controller.py
@@ -1,7 +1,7 @@
 
 
 from tendril.utils.db import with_db
-from .model import Artefact
+from .model import ArtefactModel
 
 from tendril.utils import log
 logger = log.get_logger(__name__, log.DEFAULT)
@@ -9,7 +9,7 @@ logger = log.get_logger(__name__, log.DEFAULT)
 
 @with_db
 def get_artefact(id, session=None):
-    q = session.query(Artefact).filter_by(id=id)
+    q = session.query(ArtefactModel).filter_by(id=id)
     return q.one()
 
 
@@ -17,17 +17,33 @@ def get_artefact(id, session=None):
 def create_artefact(type, session=None):
     if not type:
         raise ValueError("type cannot be None")
-    artefact = Artefact(type=type)
+    artefact = ArtefactModel(type=type)
     session.add(artefact)
     return artefact
 
 
 @with_db
-def register_artefact(artefact: Artefact, session=None):
+def register_artefact(artefact: ArtefactModel, session=None):
     session.add(artefact)
     return artefact
+
+
+def preprocess_artefact(artefact):
+    if isinstance(artefact, int):
+        return artefact
+    if isinstance(artefact, ArtefactModel):
+        return artefact.id
+
+
+@with_db
+def get_artefact_owner(artefact, session=None):
+    artefact_id = preprocess_artefact(artefact)
+    artefact = get_artefact(artefact, session=session)
+    return artefact.user
 
 
 @with_db
 def creare_artefact_log_entry(artefact_id, action, reference=None, session=None):
     pass
+
+
