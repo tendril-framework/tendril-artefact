122d9ef84af846bf83e73bdc221cdace9173301a
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2024-04-21 17:51:19 +0530

    Add db controller for interest artefacts

--- src/tendril/artefacts/db/controller.py
+++ src/tendril/artefacts/db/controller.py
@@ -1,16 +1,44 @@
 
 
+from sqlalchemy.exc import NoResultFound
 from tendril.utils.db import with_db
 from .model import ArtefactModel
 
 from tendril.utils import log
-logger = log.get_logger(__name__, log.DEFAULT)
+logger = log.get_logger(__name__)
 
 
 @with_db
 def get_artefact(id, session=None):
     q = session.query(ArtefactModel).filter_by(id=id)
     return q.one()
+
+
+def _type_discriminator(type):
+    if not type:
+        return ArtefactModel
+    # if isinstance(type, str):
+    #     from tendril.interests import type_codes
+    #     return type_codes[type].model
+    if issubclass(type, ArtefactModel):
+        return type
+    logger.warn(f"Did not find a Model Class for Artefact type {type}")
+    return ArtefactModel
+
+
+@with_db
+def get_interest_artefacts(interest_id, type=None, label=None, session=None):
+    filters = []
+    qmodel = _type_discriminator(type)
+    if interest_id:
+        filters.append(qmodel.interest_id == interest_id)
+    if label:
+        filters.append(qmodel.label == label)
+    q = session.query(qmodel).filter(*filters)
+    try:
+        return q.all()
+    except NoResultFound:
+        return None
 
 
 @with_db
